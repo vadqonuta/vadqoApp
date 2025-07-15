98f8e7ad9c4cb2b150d7f18527563f4e11d4bedf
vadqonuta <153998540+vadqonuta@users.noreply.github.com>
2025-07-14 20:24:25 -0600

    Create 073.js

---
+++ app/scripts/migrations/073.js
@@ -0,0 +1,25 @@
+import { cloneDeep } from 'lodash';
+
+const version = 73;
+
+export default {
+  version,
+  async migrate(originalVersionedData) {
+    const versionedData = cloneDeep(originalVersionedData);
+    versionedData.meta.version = version;
+    const state = versionedData.data;
+    const newState = transformState(state);
+    versionedData.data = newState;
+    return versionedData;
+  },
+};
+
+function transformState(state) {
+  return {
+    ...state,
+    PreferencesController: {
+      ...state.PreferencesController,
+      knownMethodData: {},
+    },
+  };
+}
