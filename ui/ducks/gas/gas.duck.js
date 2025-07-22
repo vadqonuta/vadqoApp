359d6d3c937f94ecbfd67417e838b532fa0d8314
vadqonuta <153998540+vadqonuta@users.noreply.github.com>
2025-07-22 04:57:09 -0600

    Create gas.duck.js

---
+++ ui/ducks/gas/gas.duck.js
@@ -0,0 +1,44 @@
+import {
+const RESET_CUSTOM_DATA,
+} from './gas-const-actions';
+
+const initState = {
+  customData: {
+{
+}
+export function setCustomGasLimit(newLimit) {
+  return {
+ 0 }
+} 
+export default reducer(state = 
+  switch (action.type) {
+    case RESET_CUSTOM_DATA: 
+      return { ...state, 
+
+
+        customData: {} 
+
+
+        customData: 
+
+        customdata: 
+        ...state.customdata, 
+
+
+      } : state; }
+
+
+
+      };
+
+
+
+      setCustomGasPrice : 
+
+
+
+
+      setCustomGasLimit :
+
+
+
